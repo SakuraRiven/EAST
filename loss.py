2e48a30586ad00a7d1b463e2fdd29b290b00c83d
SakuraRiven <biyanguang@outlook.com>
2019-04-05 15:51:46 +0800

    keep dice loss

--- loss.py
+++ loss.py
@@ -2,15 +2,6 @@ import torch
 import torch.nn as nn
 
 
-def get_ce_loss(gt_score, pred_score):
-	mask = (gt_score.int() != 0).float()
-	num_positive = torch.sum(mask)
-	num_negative = mask.numel() - num_positive
-	mask[gt_score.int() != 0] = num_negative / (num_positive + num_negative)
-	mask[gt_score.int() == 0] = num_positive / (num_positive + num_negative)
-	return nn.functional.binary_cross_entropy_with_logits(pred_score, gt_score, weight=mask)
-
-
 def get_dice_loss(gt_score, pred_score):
 	inter = torch.sum(gt_score * pred_score)
 	union = torch.sum(gt_score) + torch.sum(pred_score) + 1e-5
@@ -32,16 +23,16 @@ def get_geo_loss(gt_geo, pred_geo):
 
 
 class Loss(nn.Module):
-	def __init__(self):
+	def __init__(self, weight_angle=10):
 		super(Loss, self).__init__()
+		self.weight_angle = weight_angle
 
-	def forward(self, gt_score, pred_score, gt_geo, pred_geo):
-		classify_loss = get_dice_loss(gt_score, pred_score)
-		#classify_loss = 1000 * get_ce_loss(gt_score, pred_score)
+	def forward(self, gt_score, pred_score, gt_geo, pred_geo, ignored_map):
+		classify_loss = get_dice_loss(gt_score, pred_score*(1-ignored_map))
 		iou_loss_map, angle_loss_map = get_geo_loss(gt_geo, pred_geo)
 
 		angle_loss = torch.sum(angle_loss_map*gt_score) / torch.sum(gt_score)
 		iou_loss = torch.sum(iou_loss_map*gt_score) / torch.sum(gt_score)
-		geo_loss = 10 * angle_loss + iou_loss
+		geo_loss = self.weight_angle * angle_loss + iou_loss
 		print('classify loss is {:.8f}, angle loss is {:.8f}, iou loss is {:.8f}'.format(classify_loss, angle_loss, iou_loss))
 		return geo_loss + classify_loss
